b8c43b270bd3b3aebce6aa69e2b160f79cd3002c
pouple <antonin.verdier@laposte.net>
2021-10-16 02:13:59 +0200

    add settings

---
+++ Elphy_Analysis/settings.py
@@ -0,0 +1,10 @@
+import os
+
+class Batch():
+	def __init__(self):
+		self.id_first_batch = ['660459', '660461', '660462', '660463', '660267', '660268', '660269']
+		self.id_second_batch = ['693043', '693044', '693046', '693047', '693048', '693049', '693050']
+		self.id_first_collab = ['682348', '682349', '682350', '682351', '682352', '682353', '682354']
+
+		self.dead_first_batch = ['660463']
+		self.dead_second_batch = ['693045']
