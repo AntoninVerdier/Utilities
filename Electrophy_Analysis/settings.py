9158450bceac9567dc93439365f2833100038cf0
pouple <antonin@verdier.fr>
2021-11-22 21:56:02 +0100

    update settings

--- Electrophy_Analysis/settings.py
+++ Electrophy_Analysis/settings.py
@@ -4,7 +4,6 @@ class paths():
 	def __init__(self):
 		self.Ksdir = '/home/user/Documents/Antonin/DataEphys/Antonin_08112021_2/Antonin_08112021__211108_183744/'
 		self.SoundInfo = '/home/user/Documents/Antonin/DataEphys/20211108T18_37_57/SoundInfo.mat'
-		self.digitalin = '/home/user/Documents/Antonin/DataEphys/Antonin_08112021_2/Antonin_08112021__211108_183744/digitalin.dat'
 		self.Output = os.path.join(self.Ksdir, 'Output')
 
 		if not os.path.exists(self.Output):
