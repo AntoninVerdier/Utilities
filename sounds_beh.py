04b2cef4a9e97bc762138e17f49df30f2935ec3d
Antonin Verdier <antonin.verdier@laposte.net>
2021-12-23 10:15:38 +0100

    add sound beh generation

--- sounds_beh.py
+++ sounds_beh.py
@@ -1,39 +1,50 @@
+import os
 import QuickSound.Sound as Sound
 import numpy as np
 
 samplerate = 192000
 amplitude = 70
-path = ''
+path = 'Samples'
+
+if not os.path.exists(path):
+	os.makedirs(path)
 
 # Generate sounds for frequency discrimnation
-# for f in np.geomspace(6e3, 16e3, 16):
-# 	pure = Sound(samplerate=samplerate, amplitude=amplitude)
-# 	pure.pure_tone(f, duration=4000)
-# 	pure.save_wav(name='PT_{0}Hz_{1}ms_{2}dB'.format(int(f), 4000, amplitude), path=path)
+for f in np.geomspace(6e3, 16e3, 16):
+	pure = Sound(samplerate=samplerate, amplitude=amplitude)
+	pure.pure_tone(f, duration=duration)
+	pure.save_wav(name='PT_{0}Hz_{1}ms_{2}dB'.format(int(f), duration, amplitude), path=path)
 
 # Generate sounds for amplitude modulation
-# for a in np.geomspace(20, 200, 16):
-# 	pure = Sound(samplerate=samplerate, amplitude=amplitude)
-# 	pure.amplitude_modulation(10e3, a, duration=4000)
-# 	pure.save_wav(name='AM_{0}Hz_{1}ms_{2}dB'.format(int(a), 4000, amplitude), path=path)
+for a in np.geomspace(20, 200, 16):
+	pure = Sound(samplerate=samplerate, amplitude=amplitude)
+	pure.amplitude_modulation(10e3, a, duration=duration)
+	pure.save_wav(name='AM_{0}Hz_{1}ms_{2}dB'.format(int(a), duration, amplitude), path=path)
 
 # Generates sounds for amplitude modulation and noise
-# for na in [45, 50, 55, 60]:
-# 	for a in np.geomspace(20, 200, 6):
-# 		pure = Sound(samplerate=samplerate, amplitude=amplitude)
-# 		pure.amplitude_modulation(10e3, a, duration=4000)
-# 		noise = Sound(samplerate=samplerate, amplitude=na)
-# 		noise.noise(duration=4000)
-# 		final = pure * noise
-# 		final.save_wav(name='AMN_{0}Hz_{1}ms_{2}dB_noise_{3}dB'.format(int(a), 4000, amplitude, na), path=path)
+for na in [45, 50, 55, 60]:
+	for a in np.geomspace(20, 200, 6):
+		pure = Sound(samplerate=samplerate, amplitude=amplitude)
+		pure.amplitude_modulation(10e3, a, duration=duration)
+		noise = Sound(samplerate=samplerate, amplitude=na)
+		noise.noise(duration=duration)
+		final = pure * noise
+		final.save_wav(name='AMN_{0}Hz_{1}ms_{2}dB_noise_{3}dB'.format(int(a), duration, amplitude, na), path=path)
 
 # Generates a chirp
-# chirp = Sound(samplerate=samplerate, amplitude=amplitude)
-# chirp.freq_modulation(6e3, 16e3, duration=4000)
-# chirp.save_wav(name='Chirp_6000Hz_16000Hz_4000ms_70dB', path=path)
+chirp = Sound(samplerate=samplerate, amplitude=amplitude)
+chirp.freq_modulation(6e3, 16e3, duration=duration)
+chirp.save_wav(name='Chirp_6000Hz_16000Hz_{}ms_70dB'.format(duration), path=path)
 
 # Generates steps
-# for s in [2, 4, 6, 8, 10]:
-# 	step = Sound(samplerate=samplerate, amplitude=amplitude)
-# 	step.steps(6e3, 16e3, s, duration=4000)
-# 	step.save_wav(name='Steps_{}_6000Hz_16000Hz_4000ms_70dB'.format(s), path=path)
+for s in [2, 4, 6, 8, 10]:
+	step = Sound(samplerate=samplerate, amplitude=amplitude)
+	step.steps(6e3, 16e3, s, duration=duration)
+	step.save_wav(name='Steps_{}_6000Hz_16000Hz_{}ms_70dB'format(s, duration), path=path)
+
+# Generate harmonic 
+struc = [(4e3,), (4e3, 20e3), (4e3, 12e3, 20e3), (4e3, 8e3, 12e3, 20e3), (4e3, 8e3, 12e3, 16e3, 20e3)]
+for h in struct:
+	for f in h:
+		pure = Sound(samplerate=samplerate, amplitude=amplitude)
+		pure.pure_tone(f, duration=500)
